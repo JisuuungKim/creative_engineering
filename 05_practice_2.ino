1d63800bd09822e808ab6493ecad97e559b6bbd7
JisuuungKim <cham0810@kookmin.ac.kr>
2020-09-20 17:32:58 +0900

    3week[05_practice_2]수정

--- 05_practice_2.ino
+++ 05_practice_2.ino
@@ -1,26 +1,11 @@
 
 #define PIN_LED 7
 
-unsigned int count, toggle;
 int sum = 0;
 
 void setup() {
 
-  pinMode(PIN_LED, OUTPUT)
-
-  ; Serial.begin(115200); // Initialize serial port
-
-  while (!Serial) {
-
-    ; // wait for serial port to connect.
-
-  }
-
-  Serial.println("Hello World!");
-
-  count = toggle = 0;
-
-  digitalWrite(PIN_LED, toggle); // turn off LED.
+  pinMode(PIN_LED, OUTPUT);
 
 }
 
